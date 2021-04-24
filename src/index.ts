f6ec0041dd1fe7f35b923f8960269748c2b2542e
Vis Virial <visvirial@gmail.com>
2021-04-24 15:04:46 +0900

    Determine address type automatically.

--- src/index.ts
+++ src/index.ts
@@ -126,13 +126,52 @@ export class CryptoIncognito {
 		return await epir.reply_decrypt(reply, this.privKey, dimension, packing);
 	}
 	
+	static decodeAddress(address: string): { buf: Buffer, addrType: string } | null {
+		try {
+			const base58 = bitcoin.address.fromBase58Check(address);
+			const buf = base58.hash;
+			const addrType = (
+				base58.version == 0x00 || base58.version == 0x6f ? 'p2pkh' :
+					base58.version == 0x05 || base58.version == 0xc4 ? 'p2sh' : null);
+			if(!addrType) return null;
+			return {
+				buf: buf,
+				addrType: addrType,
+			};
+		} catch(e) {
+			try {
+				const bech32 = bitcoin.address.fromBech32(address);
+				const buf = bech32.data;
+				if(bech32.version == 0) {
+					const addrType = (buf.length == 20 ? 'p2wpkh' : buf.length == 32 ? 'p2wsh' : null);
+					if(!addrType) return null;
+					return {
+						buf: buf,
+						addrType: addrType,
+					};
+				} else if(bech32.version == 1) {
+					if(buf.length != 32) return null;
+					return {
+						buf: buf,
+						addrType: 'p2tr',
+					};
+				} else {
+					return null;
+				}
+			} catch(e) {
+				return null;
+			}
+		}
+	}
+	
 	// Conduct a PIR binary search to find the location of the specified address and retrieves all UTXOs matching the address.
-	async findUTXOs(coin: string, addrType: string, address: string): Promise<UTXOEntry[]> {
+	async findUTXOs(coin: string, address: string): Promise<UTXOEntry[]> {
 		const time = () => new Date().getTime();
 		// Decode address.
-		const addrBuf = (['p2pkh', 'p2sh'].indexOf(addrType) >= 0) ?
-			bitcoin.address.fromBase58Check(address).hash :
-			bitcoin.address.fromBech32(address).data;
+		const decodedResult = CryptoIncognito.decodeAddress(address);
+		if(!decodedResult) throw new Error('Failed to determine address type.');
+		const addrBuf = decodedResult.buf;
+		const addrType = decodedResult.addrType;
 		// Fetch UTXOSetInfo.
 		const utxoSetInfoAddress = await this.getUTXOSetInfo(coin, addrType, 'address');
 		const utxoSetInfoRange = await this.getUTXOSetInfo(coin, addrType, 'range');
